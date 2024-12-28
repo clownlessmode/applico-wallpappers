4c4c1fc88446ff2d765129d165c9c0aa2f6f3187
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2024-12-28 12:29:55 +0700

    fix

---
+++ src/app/api/bot/route.ts
@@ -0,0 +1,18 @@
+export const dynamic = 'force-dynamic';
+import process from 'process';
+
+export const fetchCache = 'force-no-store';
+
+import { Bot, webhookCallback } from 'grammy';
+
+const token = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
+
+if (!token)
+  throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.');
+
+const bot = new Bot(token);
+bot.on('message:text', async (ctx) => {
+  await ctx.reply(ctx.message.text);
+});
+
+export const POST = webhookCallback(bot, 'std/http');
